e744c9992fa1f3b47e17e499c90224ba6fa47774
dandan1232 <345652127@qq.com>
2021-05-08 01:36:18 +0800

    微服务网关Gateway

---
+++ src/test/java/online/danbao/servicegateway/ServiceGatewayApplicationTests.java
@@ -0,0 +1,15 @@
+package online.danbao.servicegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootTest
+@EnableDiscoveryClient
+class ServiceGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
